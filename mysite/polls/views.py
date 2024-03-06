dc00d17623a2a5b81df766124cd19fc65163207d
rta2105 <rta2105@gmail.com>
2024-03-06 09:12:03 -0300

    aula 06_03

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("Você esta vendo uma pergunta %s." % question_id)
+
+
+def results(request, question_id):
+    response = "Vc esta vendo o resultado %s."
+    return HttpResponse(response %question_id)
+
+
+def vote(request, question_id):
+	return HttpResponse("Vc esta vendo o voto em uma pergunta %s." % question_id)
